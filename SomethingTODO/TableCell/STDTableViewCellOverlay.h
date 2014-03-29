ce1779ec9348b755335d9877dd99774786f625cf
Alex Núñez <atb1983@gmail.com>
2014-03-29 08:49:35 +0100

    Added the task completed gestures and overlay for the tableviecell

---
+++ SomethingTODO/TableCell/STDTableViewCellOverlay.h
@@ -0,0 +1,13 @@
+//
+//  STDTableViewCellOverlay.h
+//  SomethingTODO
+//
+//  Created by Alex Núñez on 29/03/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STDTableViewCellOverlay : UIView
+
+@end
---
+++ SomethingTODO/TableCell/STDTableViewCellOverlay.m
@@ -0,0 +1,31 @@
+//
+//  STDTableViewCellOverlay.m
+//  SomethingTODO
+//
+//  Created by Alex Núñez on 29/03/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import "STDTableViewCellOverlay.h"
+
+@implementation STDTableViewCellOverlay
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
