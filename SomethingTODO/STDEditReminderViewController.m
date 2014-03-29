7ca6aa4e19785818b5ece3442e461a2372945127
Alex Núñez <atb1983@gmail.com>
2014-03-29 12:15:40 +0100

    Added the Edit Reminder VC and the connection between the Master VC

---
+++ SomethingTODO/STDEditReminderViewController.m
@@ -0,0 +1,13 @@
+//
+//  STDEditReminderViewController.m
+//  SomethingTODO
+//
+//  Created by Alex Núñez on 29/03/14.
+//  Copyright (c) 2014 Alex Franco. All rights reserved.
+//
+
+#import "STDEditReminderViewController.h"
+
+@implementation STDEditReminderViewController
+
+@end
